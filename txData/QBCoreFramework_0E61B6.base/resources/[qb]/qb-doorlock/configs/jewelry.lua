672515457bc8b218e04a339b1bb1fbe6108a0d55
abdel1touimi <abdel1.touimi@gmail.com>
2022-09-03 11:42:07 +0100

    pawn shop map + doorlock jewelry

---
+++ txData/QBCoreFramework_0E61B6.base/resources/[qb]/qb-doorlock/configs/jewelry.lua
@@ -0,0 +1,15 @@
+
+
+-- jewelrydoor1 created by PassPar2
+Config.DoorList['jewelry-jewelrydoor1'] = {
+    doorType = 'double',
+    objYaw = 306.00003051758,
+    doorRate = 1.0,
+    objCoords = vec3(-630.426514, -238.437546, 38.206532),
+    distance = 2,
+    fixText = false,
+    doorLabel = 'jewelrydoor1',
+    locked = false,
+    objName = 9467943,
+    pickable = true,
+}
