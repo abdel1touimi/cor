4eda57d1692dd494ee8bbc18f7517f45381e42f6
abdel1touimi <abdel1.touimi@gmail.com>
2022-09-18 17:26:35 +0100

    chmod ressources for dev purpose

--- txData/QBCoreFramework_0E61B6.base/resources/[additional]/pp2-pickupbmx/client.lua
+++ txData/QBCoreFramework_0E61B6.base/resources/[additional]/pp2-pickupbmx/client.lua
@@ -1,12 +1,10 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 
 local bmxID = 1131912276
-local models = {
-    'bmx',
-}
+local bmxModel = 'bmx'
 local attachedBMX = false;
 
-exports['qb-target']:AddTargetModel(models, {
+exports['qb-target']:AddTargetModel(bmxModel, {
 	options = {
 		{
       targeticon = 'fas fa-bicycle', 
@@ -30,9 +28,13 @@ local function LoadThaAnim(dict)
     return true
 end
 
-local function attachBMXToPlayer(bmxEntity)
-  AttachEntityToEntity(bmxEntity, PlayerPedId(), GetPedBoneIndex(PlayerPedId(), 24818), 0.08, -0.25, 0.0, 10.0, 10.0, 90.0, false, false, false, false, 2, true)
-  attachedBMX = bmxEntity
+local function RemoveBmxFromScene(entity)
+  NetworkRegisterEntityAsNetworked(entity)
+  Wait(100)
+  NetworkRequestControlOfEntity(entity)
+  SetEntityAsMissionEntity(entity)
+  Wait(100)
+  DeleteEntity(entity)
 end
 
 RegisterNetEvent('pp2-pickupbmx:client:fetchBMX', function(bmxEntity)
@@ -42,16 +44,11 @@ RegisterNetEvent('pp2-pickupbmx:client:fetchBMX', function(bmxEntity)
     local colorPrimary, colorSecondary = GetVehicleColours(bmxEntity)
     local perl, wheel = GetVehicleExtraColours(bmxEntity)
     local plate = GetVehicleNumberPlateText(bmxEntity)
-    NetworkRequestControlOfEntity(bmxEntity)
     LoadThaAnim('anim@mp_snowball')
     TaskPlayAnim(playerPed, 'anim@mp_snowball', 'pickup_snowball', 8.0, 8.0, -1, 48, 1, false, false, false)
     TriggerServerEvent('pp2-pickupbmx:server:StoreBMX', 'bmx', colorPrimary, colorSecondary, perl, plate)
+    RemoveBmxFromScene(bmxEntity)
     Wait(1000)
-    if Config.EnableAttach then
-      attachBMXToPlayer(bmxEntity)
-    else
-      DeleteEntity(bmxEntity)
-    end
   else
     QBCore.Functions.Notify('You already have a bike on you.', 'error', 5000)
   end
@@ -63,16 +60,11 @@ RegisterNetEvent('pp2-pickupbmx:client:placeBMX', function(prim,sec,perl,plate)
   local coords = GetEntityCoords(playerPed) + forward * 1
   LoadThaAnim('anim@mp_snowball')
   TaskPlayAnim(playerPed, 'anim@mp_snowball', 'pickup_snowball', 8.0, 8.0, -1, 48, 1, false, false, false)
-  if attachedBMX and Config.EnableAttach then
-    DeleteEntity(attachedBMX)
-    attachedBMX = false
-  end
   Wait(1000)
   while not HasModelLoaded(bmxID) do
     RequestModel(bmxID)
     Citizen.Wait(10)
   end
-
   if HasModelLoaded(bmxID) then
     local createdbmx = CreateVehicle(bmxID, coords, 1.0, true, true)
     if createdbmx ~= 0 then
@@ -82,4 +74,28 @@ RegisterNetEvent('pp2-pickupbmx:client:placeBMX', function(prim,sec,perl,plate)
       SetEntityHeading(createdbmx, GetEntityHeading(playerPed))
     end
   end
+end)
+
+RegisterNetEvent('QBCore:Client:OnPlayerLoaded', function()
+  local playerPed = PlayerPedId()
+	while true do
+		Wait(100)
+		if Config.EnableAttach then
+      local hasItem = QBCore.Functions.HasItem('bmx')
+      if hasItem and not attachedBMX then
+        while not HasModelLoaded(bmxID) do
+          RequestModel(bmxID)
+          Citizen.Wait(10)
+        end
+        local forward = GetEntityForwardVector(playerPed)
+        local coords = GetEntityCoords(playerPed) + forward * 1
+        local createdbmx = CreateVehicle(bmxID, coords, 1.0, true, true)
+        AttachEntityToEntity(createdbmx, PlayerPedId(), GetPedBoneIndex(PlayerPedId(), 24818), 0.08, -0.25, 0.0, 10.0, 10.0, 90.0, false, false, false, false, 2, true)
+        attachedBMX = createdbmx
+      elseif not hasItem and attachedBMX then
+        RemoveBmxFromScene(attachedBMX)
+        attachedBMX = false
+      end
+    end
+	end
 end)
